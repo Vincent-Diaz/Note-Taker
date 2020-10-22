32ba44e9dd3a7942d615c61d9345cadfa7c0eaf3
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-22 16:20:54 -0500

    corrected syntax error

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
+//const fs = require("fs");
+//const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json);
+app.use(express.json());
 app.use(express.static("public"));
 
-require("./public/routes/APIroutes")(app);
-require("./public/routes/HTMLroutes")(app);
+require("./routes/HTMLroutes")(app);
+require("./routes/APIroutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
