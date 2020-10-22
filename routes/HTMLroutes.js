ddd7a94cbc02d7b29cbe78d08f8685416a4e66d2
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-22 16:21:31 -0500

    updated app.get order

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -1,13 +1,14 @@
 const path = require("path");
 
 module.exports = function(app) {
-
-    app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "./public/index.html"));
-    });
     
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+        // console.log("home");
     });
 
 }
